bdba88cac40bdf2845fc9149cd7f7c7926d0e457
ksupasate <48704858+ksupasate@users.noreply.github.com>
2020-02-09 13:32:52 +0700

    Add new file

---
+++ BMI/BMI/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim height, weight, BMI As Single
+        height = TextBox2.Text
+        weight = TextBox1.Text
+        BMI = (weight) / (height ^ 2)
+        TextBox3.Text = BMI
+
+
+    End Sub
+End Class
